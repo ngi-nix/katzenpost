c3fb4b59883cc9430a261c1f5e879a28e8816b9e
Magic_RB <magic_rb@redalder.org>
2021-08-30 12:00:16 +0200

    Fix app definition
    Signed-off-by: Magic_RB <magic_rb@redalder.org>

--- flake.nix
+++ flake.nix
@@ -13,48 +13,62 @@
       forAllSystems' = systems: fun: nixpkgs.lib.genAttrs systems fun;
       forAllSystems = forAllSystems' supportedSystems;
     in
-    {
-      overlays.katzenpost = final: prev:
-        {
-          katzenpost-server = final.callPackage ./packages/katzenpost-server.nix {}; 
-          katzenpost-authority = final.callPackage ./packages/katzenpost-authority.nix {}; 
-          catshadow = final.callPackage ./packages/catshadow.nix {}; 
-          catchat = final.callPackage ./packages/catchat.nix {}; 
-        };
-
-      hydraJobs = forAllSystems (system:
-        let
-          pkgs = self.packages.${system};
-        in {
-          build-katzenpost-server = pkgs.katzenpost-server;
-          build-katzenpost-authority-voting = pkgs.katzenpost-authority.override { voting = true; };
-          build-katzenpost-authority-nonvoting = pkgs.katzenpost-authority.override { voting = false; };
-        });
-
-      defaultPackage = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; overlays = [ gomod2nix.overlay self.overlays.katzenpost ]; };
-        in
-          pkgs.symlinkJoin
-            { name = "katzenpost";
-              paths = with pkgs; 
-                [ katzenpost-server
-                  katzenpost-authority
-                  catchat
-                ];
-            }
-      ); 
-
-      packages = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; overlays = [ gomod2nix.overlay self.overlays.katzenpost ]; };
-        in
+      with nixpkgs.lib;
+      {
+        overlays.katzenpost = final: prev:
           {
-            inherit (pkgs)
-              katzenpost-server
-              katzenpost-authority
-              catchat;
-          }
-      );
-    };
+            katzenpost-server = final.callPackage ./packages/katzenpost-server.nix {};
+            katzenpost-authority = final.callPackage ./packages/katzenpost-authority.nix {};
+            catshadow = final.callPackage ./packages/catshadow.nix {};
+            catchat = final.callPackage ./packages/catchat.nix {};
+          };
+
+        hydraJobs = forAllSystems (system:
+          let
+            pkgs = self.packages.${system};
+          in {
+            build-katzenpost-server = pkgs.katzenpost-server;
+            build-katzenpost-authority-voting = pkgs.katzenpost-authority.override { voting = true; };
+            build-katzenpost-authority-nonvoting = pkgs.katzenpost-authority.override { voting = false; };
+          });
+
+        defaultPackage = forAllSystems (system:
+          let
+            pkgs = import nixpkgs { inherit system; overlays = [ gomod2nix.overlay self.overlays.katzenpost ]; };
+          in
+            pkgs.symlinkJoin
+              { name = "katzenpost";
+                paths = with pkgs;
+                  [ katzenpost-server
+                    katzenpost-authority
+                    catchat
+                  ];
+              }
+        );
+
+        packages = forAllSystems (system:
+          let
+            pkgs = import nixpkgs { inherit system; overlays = [ gomod2nix.overlay self.overlays.katzenpost ]; };
+          in
+            {
+              inherit (pkgs)
+                katzenpost-server
+                katzenpost-authority
+                catchat;
+            }
+        );
+
+        apps = mapAttrs (_: v:
+          mapAttrs (_: a:
+            {
+              type = "app";
+              program = a;
+            }
+          ) v
+        ) self.packages;
+
+        defaultApp = mapAttrs (_: v:
+          v.catchat
+        ) self.apps;
+      };
 }
